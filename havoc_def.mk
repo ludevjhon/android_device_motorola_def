932792789d9217c7ce48f4fa8587a4c9220cea72
ludevjhon <70524137+ludevjhon@users.noreply.github.com>
2020-09-08 11:06:00 -0300

    Update havoc_def.mk

--- havoc_def.mk
+++ havoc_def.mk
@@ -33,7 +33,7 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := sanders
+PRODUCT_DEVICE := Moto one hyper
 PRODUCT_NAME := havoc_def
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
